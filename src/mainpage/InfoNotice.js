2d2bd7f72c7d9baef0542827bcf412ef0320e902
Sihyun <yellownyou@gmail.com>
2023-02-24 18:07:25 +0900

    finish

---
+++ src/mainpage/InfoNotice.js
@@ -0,0 +1,48 @@
+import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";	// 추가
+import { Swiper, SwiperSlide } from 'swiper/react';
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
+import "./mainpage.css"
+import { Link } from "react-router-dom";
+
+SwiperCore.use([ Autoplay])	// 추가
+
+function InfoNotice({annoucement}){
+    return(
+        <div className="info-notice">
+        <h2>공지사항</h2>
+        <div className="border" >
+            <div className="wrap">
+                <Swiper className="banner"
+                    spaceBetween={50}
+                    slidesPerView={1}
+                    autoplay={{ delay: 5000 }}>
+                    <div className="swiper-wrapper" >
+                        {console.log(annoucement)}
+                        {
+                            annoucement && annoucement.map(a => (
+                                <SwiperSlide>
+                                    <p className="tit" style={{padding:21}}>지점</p>
+                                    <p className="link" style={{padding:21}}>
+                                        <a href="#" title="공지사항 상세보기">
+                                            <strong>
+                                                [종로]
+                                            </strong>
+                                            {a.title}</a>
+                                    </p>
+
+                                    <div><p className="date" style={{padding:21 ,marginRight:'35px'}}>{a.announcementDate}</p></div>
+                                   <div> <p className="more" style={{padding:21}}>
+                                        <Link to="/annoucemount" style={{color:'black'}} title="전체공지 더보기">더보기<i className="bi bi-chevron-right"></i></Link>
+                                    </p></div>
+                                </SwiperSlide>
+                            ))
+                        }
+                    </div>
+                </Swiper>
+            </div>
+        </div>
+    </div>
+    )
+}export default InfoNotice
